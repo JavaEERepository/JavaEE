e199dfa2fff000be5c0fe970072c410106ebef92
Wu <Wu@DESKTOP-CG8S896>
2018-09-12 11:53:18 +0800

    吴帅提交的部分

--- src/test/java/test/HelloWorldFromW.java
+++ src/test/java/test/HelloWorldFromW.java
@@ -1,11 +1,14 @@
-package test;
 
-public class HelloWorldFromW {
-	public void main(){
-		this.wu();
+	package test;
+
+	public class HelloWorldFromW {
+		public void main(){
+			this.wu();
+		}
+		
+		public void wu(){
+			System.out.println("i am W , Hello World");
+		}
 	}
-	
-	public void wu(){
-		System.out.println("i am W , Hello World");
-	}
-}
+
+
