c52e9d1681622e52b53d4731d1e6229288e0fa1e
asus <asus@DESKTOP-P9S1VFB>
2018-09-12 12:04:38 +0800

    郑文杰提交的部分helloWorld 3.0

--- src/test/java/test/HelloWorldFromZ.java
+++ src/test/java/test/HelloWorldFromZ.java
@@ -3,15 +3,15 @@ package test;
 public class HelloWorldFromZ {
 	
 	public void HelloWorld1() {
-		System.out.println("HelloWorld!!!");
+		System.out.println("HelloWorld1!!!");
 	}
 	
 	public void HelloWorld2() {
-		System.out.println("HelloWorld!!!");
+		System.out.println("HelloWorld2!!!");
 	}
 	
 	public void HelloWorld3() {
-		System.out.println("HelloWorld!!!");
+		System.out.println("HelloWorld3!!!");
 	}
 	
 }
